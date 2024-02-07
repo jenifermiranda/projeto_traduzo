be5880c8911e9bff8d0eb3c7ccdf88af580a2ced
jenifer miranda <jenifermiranda@MBPdegabriel2.Home>
2024-02-07 17:19:17 +0000

    feat: implementa o metodo to_list() e o metodo list_dicts()

--- src/models/language_model.py
+++ src/models/language_model.py
@@ -3,7 +3,16 @@ from models.abstract_model import AbstractModel
 
 
 class LanguageModel(AbstractModel):
-    _colection = db["languages"]
+    _collection = db["languages"]
 
     def __init__(self, data: dict):
         super().__init__(data)
+
+    def to_dict(self):
+        return {"name": self.data["name"], "acronym": self.data["acronym"]}
+
+    @classmethod
+    def list_dicts(cls):
+        data = cls.find()
+        for language in data:
+            return language.to_dict()
