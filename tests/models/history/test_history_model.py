f07297f6b399c5d2f8ca1354aa27446a6ed94604
jenifer miranda <jenifermiranda@MBPdegabriel2.Home>
2024-02-08 22:24:44 +0000

    tests

--- tests/models/history/test_history_model.py
+++ tests/models/history/test_history_model.py
@@ -3,5 +3,13 @@
 
 
 # Req. 7
+from flask import json
+from src.models.history_model import HistoryModel
+
+
 def test_request_history():
-    raise NotImplementedError
+    history_list = json.loads(HistoryModel.list_as_json())
+
+    assert history_list[0]["text_to_translate"] == "Hello, I like videogame"
+    assert history_list[0]["translate_from"] == "en"
+    assert history_list[0]["translate_to"] == "pt"
